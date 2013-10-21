f9f08628da05b70bdbb7a50276e7803e7c591e7b
Rafael Erthal <rafaerthal@gmail.com>
2013-10-21 02:08:48 -0200

    creating new routes

--- src/main/java/server/services/PatrimonyService.java
+++ src/main/java/server/services/PatrimonyService.java
@@ -3,17 +3,38 @@ package server.services;
 import server.models.Patrimony;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/patrimonies")
 @Produces(MediaType.APPLICATION_JSON)
 public class PatrimonyService {
 
     @GET
-    public Patrimony get() {
+    @Path("/patrimonies")
+    public Patrimony list() {
         return new Patrimony();
+    }
+
+    @GET
+    @Path("/patrimonies/:id")
+    public Patrimony list() {
+        return new Patrimony();
+    }
+
+    @PUT
+    @Path("/patrimonies/:id")
+    public Patrimony list() {
+        return new Patrimony();
+    }
+
+    @POST
+    @Path("/patrimonies")
+    public Patrimony import() {
+        return null;
     }
 
 }
