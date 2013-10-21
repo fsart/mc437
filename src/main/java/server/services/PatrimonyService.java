b4f520a7ee3811caf0ee03e8c2aad602d6b75841
Rafael Erthal <rafaerthal@gmail.com>
2013-10-21 02:38:21 -0200

    fixing import route

--- src/main/java/server/services/PatrimonyService.java
+++ src/main/java/server/services/PatrimonyService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 public class PatrimonyService {
 
     @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Patrimony import () {
         return new Patrimony();
     }
