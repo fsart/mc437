764ceca76a7a9eeb17154868f26f88e756323f4a
Rafael Erthal <rafaerthal@gmail.com>
2013-10-21 02:31:26 -0200

    fixing details route

--- src/main/java/server/services/PatrimonyService.java
+++ src/main/java/server/services/PatrimonyService.java
@@ -3,9 +3,7 @@ package server.services;
 import server.models.Patrimony;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,7 +14,7 @@ import javax.ws.rs.core.MediaType;
 public class PatrimonyService {
 
     @POST
-    public Patrimony import () {
+    public void import () {
 
     }
 
@@ -29,12 +27,6 @@ public class PatrimonyService {
     @Path("{id}")
     public Patrimony details (@PathParam("id") String id) {
         return new Patrimony();
-    }
-
-    @PUT
-    @Path("{id}")
-    public void edit (@PathParam("id") String id) {
-
     }
 
 }
