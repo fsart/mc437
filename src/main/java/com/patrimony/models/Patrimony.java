3d3d00866b21bdb8b2b2a7630edad8f35d77e796
Rafael Erthal <rafaerthal@gmail.com>
2013-10-25 19:42:37 -0200

    morphia setup

--- src/main/java/com/patrimony/models/Patrimony.java
+++ src/main/java/com/patrimony/models/Patrimony.java
@@ -1,16 +1,15 @@
 package com.patrimony.models;
 
-import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
-import org.bson.types.ObjectId;
-
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Property;
 
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 @Entity(value="patrimonies", noClassnameStored=true)
 public class Patrimony {
 
