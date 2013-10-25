46b27b8f5bb237ddc643dad1a69bc33205af77e1
Rafael Erthal <rafaerthal@gmail.com>
2013-10-25 19:34:24 -0200

    morphia setup

--- src/main/java/com/patrimony/services/PatrimonyService.java
+++ src/main/java/com/patrimony/services/PatrimonyService.java
@@ -32,6 +32,6 @@ public class UserResources {
     @Path("/")
     @Produces("application/json")
     public Patrimony list() {
-        return new Patrimony;
+        return new Patrimony();
     }
 }
