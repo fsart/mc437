aa676b3b306799e4f4ee47e4344788523f4f3e97
Rafael Erthal <rafaerthal@gmail.com>
2013-11-01 19:13:57 -0200

    fixing try catch

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
+import java.util.Date;
+
 import java.io.File;
 import java.io.InputStream;
 import java.text.DateFormat;
