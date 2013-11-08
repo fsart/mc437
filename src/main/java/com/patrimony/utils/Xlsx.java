8951dfcc19107aedf92861cd58eeb0e3337bb163
Rafael Erthal <rafaerthal@gmail.com>
2013-11-08 20:01:26 -0200

    fixing parameters

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -38,7 +38,7 @@ public class Xlsx {
         public static final String PREFIX = "stream2file";
         public static final String SUFFIX = ".tmp";
 
-        public static File stream2file (InputStream in) throws IOException {
+        public static File parse (InputStream in) throws IOException {
             final File tempFile = File.createTempFile(PREFIX, SUFFIX);
             tempFile.deleteOnExit();
             try (FileOutputStream out = new FileOutputStream(tempFile)) {
