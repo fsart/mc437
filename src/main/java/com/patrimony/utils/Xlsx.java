74f75a6c3c9736aa2224413fca89f92e3f3b55d3
Rafael Erthal <rafaerthal@gmail.com>
2013-11-08 20:35:22 -0200

    fixing conflicts

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -16,10 +16,11 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.io.OutputStream;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
