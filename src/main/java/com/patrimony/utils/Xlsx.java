61d7407f79960d18653298b0838b2c39c6ba82cf
Rafael Erthal <rafaerthal@gmail.com>
2013-11-08 21:06:29 -0200

    fixing conflicts

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -39,23 +39,27 @@ public class Xlsx {
     }
 
     public static void read () {
-        File excel =  new File ("./temp.xlsx");
-        FileInputStream fis = new FileInputStream(excel);
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet ws = wb.getSheet("Input");
+        try {
+            File excel =  new File ("./temp.xlsx");
+            FileInputStream fis = new FileInputStream(excel);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet ws = wb.getSheet("Input");
 
-        int rowNum = ws.getLastRowNum() + 1;
-        int colNum = ws.getRow(0).getLastCellNum();
-        String [][] data = new String [rowNum] [colNum];
+            int rowNum = ws.getLastRowNum() + 1;
+            int colNum = ws.getRow(0).getLastCellNum();
+            String [][] data = new String [rowNum] [colNum];
 
-        for(int i = 0; i <rowNum; i++){
-            XSSFRow row = ws.getRow(i);
-            for (int j = 0; j < colNum; j++){
-                XSSFCell cell = row.getCell(j);
-                String value = cell.toString();
-                data[i][j] = value;
-                System.out.println ("the value is " + value);
+            for(int i = 0; i <rowNum; i++){
+                XSSFRow row = ws.getRow(i);
+                for (int j = 0; j < colNum; j++){
+                    XSSFCell cell = row.getCell(j);
+                    String value = cell.toString();
+                    data[i][j] = value;
+                    System.out.println ("the value is " + value);
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
