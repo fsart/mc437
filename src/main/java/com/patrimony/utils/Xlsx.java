ef27026b3b3e7c3c65589b5761cac8ad2ffdf459
Rafael Erthal <rafaerthal@gmail.com>
2013-11-08 21:03:43 -0200

    fixing conflicts

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -39,39 +39,23 @@ public class Xlsx {
     }
 
     public static void read () {
-        try {
-            FileInputStream file = new FileInputStream(new File("./temp.xlsx"));
+        File excel =  new File ("./temp.xlsx");
+        FileInputStream fis = new FileInputStream(excel);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet ws = wb.getSheet("Input");
 
-            //Create Workbook instance holding reference to .xlsx file
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
+        int rowNum = ws.getLastRowNum() + 1;
+        int colNum = ws.getRow(0).getLastCellNum();
+        String [][] data = new String [rowNum] [colNum];
 
-            //Get first/desired sheet from the workbook
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
-            //Iterate through each rows one by one
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                //For each row, iterate through all the columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    //Check the cell type and format accordingly
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t");
-                            break;
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "\t");
-                            break;
-                    }
-                }
-                System.out.println("");
+        for(int i = 0; i <rowNum; i++){
+            XSSFRow row = ws.getRow(i);
+            for (int j = 0; j < colNum; j++){
+                XSSFCell cell = row.getCell(j);
+                String value = cell.toString();
+                data[i][j] = value;
+                System.out.println ("the value is " + value);
             }
-            file.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
