d7fb81e70aa1090182e9e9f89c49d20db0aa79e1
Rafael Erthal <rafaerthal@gmail.com>
2013-11-08 19:54:03 -0200

    fixing parameters

--- src/main/java/com/patrimony/utils/Xlsx.java
+++ src/main/java/com/patrimony/utils/Xlsx.java
@@ -20,7 +20,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream
+import java.io.OutputStream;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
